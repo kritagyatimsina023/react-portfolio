ce85799ec00832b68236c2c1d42c89f5022f0b72
Kritagya Timsina <sahemtimsina@gmail.com>
2026-05-01 09:11:49 +0545

    change in resume

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,6 +3,7 @@ import { Planet } from "../components/Planet";
 import { Environment, Float, Lightformer } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
 import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { Suspense } from "react";
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
@@ -22,7 +23,7 @@ web experiences and products`;
         className="absolute inset-0 -z-50"
         style={{ width: "100vw", height: "100vh" }}
       >
-        <Canvas
+        {/* <Canvas
           shadows
           camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
         >
@@ -54,6 +55,35 @@ web experiences and products`;
                 form={"circle"}
                 intensity={2}
                 position={[10, 1, -0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas> */}
+        <Canvas
+          dpr={[1, 1.5]}
+          gl={{ antialias: false }}
+          camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
+        >
+          <ambientLight intensity={0.5} />
+
+          <Suspense fallback={null}>
+            <Float speed={2}>
+              <Planet scale={isMobile ? 0.7 : 1} />
+            </Float>
+          </Suspense>
+          <Environment resolution={64}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[10, 1, 0]}
                 scale={16}
               />
             </group>
